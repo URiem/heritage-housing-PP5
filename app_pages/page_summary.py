22ff6ace2caf1769e070bbaacbb8d73585a72c44
URiem <>
2023-08-17 10:27:52 +0000

    Add details to project summary page

--- app_pages/page_summary.py
+++ app_pages/page_summary.py
@@ -1,41 +1,69 @@
 import streamlit as st
+import matplotlib.pyplot as plt
+
 
 def page_summary_body():
 
-    st.write("### Quick Project Summary")
+    image_main = plt.imread(f"media/amesiowa.jpg")
+    image_isu = plt.imread(f"media/iowasu.jpg")
+
+    st.image(image_main, caption='Mainstreet - Ames, Iowa.')
+
+    st.write("### Project Summary")
 
     # text based on README file - "Dataset Content" section
     st.info(
-        f"**Project Terms & Jargon**\n"
-        f"* A **customer** is a person who consumes your service or product.\n"
-        f"* A **prospect** is a potential customer.\n"
-        f"* A **churned** customer is a user who has stopped using your product or service.\n "
-        f"* This customer has a **tenure** level, the number of months this person " 
-        f"has used our product/service.\n\n"
+        f"**Project Purpose and Motivation**\n\n"
+        f" The general pupose of this project is to provide a tool with which"
+        f" a client can predict the potential sale price of a property in"
+        f" Ames, Iowa, by providing detailed and typical information on the"
+        f" real estate in question.\n\n"
+        f" Specifically, a client has requested this app in order"
+        f" to get a reliable estimate for several inherited properties in "
+        f" Ames, Iowa. The client has provided a publically available data set"
+        f" which is used to train the machine learning model to"
+        f" predict local real estate sale prices. \n \n"
+        f"**Project Terminology**\n"
+        f"* A **client** is a person who uses this service or product.\n"
+        f"* The **sale price** is the estimated value of a home as it"
+        f" might be realized in a typical and unencumbered real estate"
+        f" transaction. \n"
+        f"* The home, the value of which is being estimated, may be refered to"
+        f" as **property, real estate, house, or home**. \n \n"
         f"**Project Dataset**\n"
-        f"* The dataset represents a **customer base from a Telco company** "
-        f"containing individual customer data on the products and services "
-        f"(like internet type, online security, online backup, tech support), "
-        f"account information (like contract type, payment method, monthly charges) "
-        f"and profile (like gender, partner, dependents).")
-
-    # Link to README file, so the users can have access to full project documentation
-    st.write(
-        f"* For additional information, please visit and **read** the "
-        f"[Project README file](https://github.com/Code-Institute-Solutions/churnometer).")
-    
+        f"* The data set can be accessed at "
+        f"[Kaggle](https://www.kaggle.com/datasets/codeinstitute/housing-prices-data)"
+        f" where it is hosted by Code Institute.\n"
+        f"* The dataset represents a record of approx. 1500 real estate "
+        f" sales in Ames, Iowa. Each record contains 23 features that"
+        f" characterize the home, it also contains the sale price."
+        f" The features are extensive, so please visit the site for more"
+        f" information.")
 
     # copied from README file - "Business Requirements" section
     st.success(
-        f"The project has 2 business requirements:\n"
-        f"* 1 - The client is interested in understanding the patterns from the customer base "
-        f"so that the client can learn the most relevant variables that are correlated to a "
-        f"churned customer.\n"
-        f"* 2 - The client is interested in determining whether or not a given prospect will churn. "
-        f"If so, the client is interested to know when. In addition, the client is "
-        f"interested in learning from which cluster this prospect will belong in the customer base. "
-        f"Based on that, present potential factors that could maintain and/or bring  "
-        f"the prospect to a non-churnable cluster."
-        )
+        f"**Business Requirements**\n\n"
+        f"The project has 3 business requirements:\n"
+        f"* 1. The client is interested in understanding the correlation "
+        f" between a properties attributes/features and the sale price."
+        f" Therefore, the client expects data visualization of the correlated"
+        f" variables against the sale prices for illustration. \n"
+        f"* 2. The client is interested in predicting the potential sale "
+        f" prices"
+        f" for properties in Ames, Iowa, and specifically, she wants to"
+        f" determine a potential value for the properties she inherited. \n"
+        f"* 3. The client would like to access the outcomes easily using"
+        f" an online application."
+    )
 
-        
+    # Link to README file, so the users can have access to full
+    # project documentation
+    st.write(
+        f"* For additional information on this project please consult the "
+        f"[README](https://github.com/URiem/heritage-housing-PP5/tree/main)"
+        f" file for this project.\n"
+        f"* For additional information on Ames, Iowa, home of Iowa State"
+        f" University and the Iowa State Cyclones, visit "
+        f"[Wikipedia](https://en.wikipedia.org/wiki/Ames,_Iowa).")
+
+    st.image(image_isu, caption='Iowa State Univesity - Ames, Iowa.')
