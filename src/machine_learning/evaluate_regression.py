0d81b98750708e08a6e8a2d63f9a6c26074c3bfd
URiem <>
2023-09-06 11:12:42 +0000

    Proofread and clean up .py files in src folder

--- src/machine_learning/evaluate_regression.py
+++ src/machine_learning/evaluate_regression.py
@@ -7,7 +7,6 @@ import pandas as pd
 
 # code copied from "Modeling and Evaluation" notebooks
 
-
 def regression_performance(X_train, y_train, X_test, y_test, pipeline):
     st.write("**Train Set**")
     regression_evaluation(X_train, y_train, pipeline)
